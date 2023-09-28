f597c7f45c908fea81b6e0e4c0936e0bc7fb1046
alexMercy <68870653+alexMercy@users.noreply.github.com>
2023-09-28 17:12:21 +0300

    add router

--- src/app/core/router.ts
+++ src/app/core/router.ts
@@ -1,70 +1,84 @@
-import {Route, routes} from "../routes.ts";
+import { Route, routes } from '../routes.ts';
 
-
-export const regExpRoutesMatcher = (path: string) => new RegExp(`^${(path)
-    .replace(/(\/?)\*/g, "($1.*)?")
-    .replace(/\/$/, "")
-    .replace(/:(\w+)(\?)?(\.)?/g, "$2(?<$1>[^/$3]+)$2$3")
-    .replace(/\.\(/g, "\\.(")}/*$`)
+export const regExpRoutesMatcher = (path: string) =>
+  new RegExp(
+    `^${path
+      .replace(/(\/?)\*/g, '($1.*)?')
+      .replace(/\/$/, '')
+      .replace(/:(\w+)(\?)?(\.)?/g, '$2(?<$1>[^/$3]+)$2$3')
+      .replace(/\.\(/g, '\\.(')}/*$`,
+  );
 
 export const getConvertedRoutes = () => {
-    const rec = (routes: Route[], baseURL = '', parent?: Route) => {
-        const res: { path: string, component: (root: HTMLElement) => void, parent: any }[] = [];
+  const rec = (routes: Route[], baseURL = '', parent?: Route) => {
+    const res: { path: string; component: (root: HTMLElement) => void; parent: any }[] = [];
 
-        routes.forEach(route => {
-            const {path, component, childs} = route;
-            res.push({path: `${baseURL}${path}`, component, parent});
-            childs && res.push(...rec(childs, path, route))
-        })
+    routes.forEach(route => {
+      const { path, component, childs, redirectTo } = route;
+      const r: any = { path: `${baseURL}${path}` };
 
-        return res;
-    }
-    const convertedRoutes = rec(routes);
-    const convPaths = convertedRoutes.map(({path}) => path);
+      component && (r.component = component);
+      redirectTo && (r.redirectTo = redirectTo);
+      parent && (r.parent = parent);
 
-    if (convPaths.length === new Set(convPaths).size) return convertedRoutes;
-    else throw new Error('Duplicate routes exist');
-}
+      res.push(r);
+      childs && res.push(...rec(childs, path, route));
+    });
+
+    return res;
+  };
+  const convertedRoutes = rec(routes);
+  const convPaths = convertedRoutes.map(({ path }) => path);
+
+  if (convPaths.length === new Set(convPaths).size) return convertedRoutes;
+  else throw new Error('Duplicate routes exist');
+};
 
 export class Router {
-    private static instance:Router;
-    private constructor(public root: HTMLElement) {}
+  private static instance: Router;
+  private constructor(public root: HTMLElement) {}
 
-    static initInstance(root: HTMLElement) {
-        if (!Router.instance) {
-            Router.instance = new Router(root);
-            this.init();
+  static initInstance(root: HTMLElement) {
+    if (!Router.instance) {
+      Router.instance = new Router(root);
+      this.init();
+    }
+  }
+
+  static getInstance() {
+    return Router.instance;
+  }
+
+  private static init() {
+    window.addEventListener('popstate', this.onPopState);
+    window.dispatchEvent(new Event('popstate'));
+  }
+
+  private static onPopState = () => {
+    const { pathname } = window.location;
+
+    const convertedRoutes = getConvertedRoutes();
+    if (!convertedRoutes) return;
+
+    const outlet: HTMLElement = Router.instance.root.querySelector('[data-outlet]')!;
+    const route = convertedRoutes.find(({ path }) => pathname.match(regExpRoutesMatcher(path)));
+
+    //TODO: Update router
+    const rec = (route: any, outlet: HTMLElement): HTMLElement => {
+      const childOutlet = route.parent ? (rec(route.parent, outlet) as HTMLElement) : outlet;
+
+      if (route.redirectTo && !route.parent) {
+        const redirectRoute = routes.find(r => r.path === route.redirectTo);
+        if (redirectRoute) {
+          // @ts-ignore
+          redirectRoute.component(childOutlet);
+          history.pushState({}, '', redirectRoute.path);
         }
-    }
+      } else route.component(childOutlet);
 
-    static getInstance() {
-        return Router.instance
-    }
-
-    private static init() {
-        window.addEventListener('popstate', this.onPopState)
-        window.dispatchEvent(new Event('popstate'));
-    }
-
-    private static onPopState = () => {
-        const {pathname} = window.location;
-
-        const convertedRoutes = getConvertedRoutes();
-        if (!convertedRoutes) return;
-
-        const outlet: HTMLElement = Router.instance.root.querySelector('[data-outlet]')!;
-        const route = convertedRoutes.find(({path}) => pathname.match(regExpRoutesMatcher(path)))
-
-        const rec = (route: any, outlet: HTMLElement): HTMLElement => {
-            const childOutlet = route.parent ? rec(route.parent, outlet) as HTMLElement : outlet;
-            route.component(childOutlet);
-            return outlet.querySelector('[data-outlet]')!
-        }
-        if(route) rec(route,outlet);
-        else throw new Error("Route Undefined")
-
-    }
-
+      return outlet.querySelector('[data-outlet]')!;
+    };
+    if (route) rec(route, outlet);
+    else throw new Error('Route Undefined');
+  };
 }
-
-
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,32 +1,37 @@
-import {HomePage} from "./pages/HomePage.ts";
-import {Page2} from "./pages/Page2.ts";
-import {IdPage} from "./pages/IdPage.ts";
+import { HomePage } from './pages/HomePage.ts';
+import { Page2 } from './pages/Page2.ts';
+import { IdPage } from './pages/IdPage.ts';
 
 export type Route = {
-    path: string,
-    component: (root: HTMLElement) => void,
-    childs?: Route[]
-}
+  path: string;
+  component?: (root: HTMLElement) => void;
+  redirectTo?: string;
+  childs?: Route[];
+};
 
 export const routes: Route[] = [
-    {
-        path: '/home',
-        component: (root) => {
-            new HomePage(root).render()
-        },
-        childs: [
-            {
-                path: '/page2',
-                component: (root: HTMLElement) => {
-                    new Page2(root).render()
-                }
-            },
-        ],
+  {
+    path: '/',
+    redirectTo: '/home',
+  },
+  {
+    path: '/home',
+    component: root => {
+      new HomePage(root).render();
     },
-    {
-        path: '/home/:id/:id2',
+    childs: [
+      {
+        path: '/page2',
         component: (root: HTMLElement) => {
-            new IdPage(root).render()
-        }
+          new Page2(root).render();
+        },
+      },
+    ],
+  },
+  {
+    path: '/home/:id/:id2',
+    component: (root: HTMLElement) => {
+      new IdPage(root).render();
     },
-]
+  },
+];
